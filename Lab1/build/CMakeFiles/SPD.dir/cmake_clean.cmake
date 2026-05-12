764be422833a34d420dc2e1d2f416488a81f4788
Jakub Szczerba <278338@student.pwr.edu.pl>
2026-05-12 09:42:23 +0200

    srodowisko Zad2

---
+++ Lab1/build/CMakeFiles/SPD.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SPD.dir/src/main.cpp.o"
+  "CMakeFiles/SPD.dir/src/main.cpp.o.d"
+  "SPD"
+  "SPD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SPD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
